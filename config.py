f97509dfca23e96f6aae8d14190e987293042a14
Fredmusicbot <106302305+Fredmusicbot@users.noreply.github.com>
2022-06-19 15:57:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,66 @@
+## What's up Kangers
+## Thanks Team Zaid 'Arigatou'
+
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv()
+admins = {}
+if str(getenv("SESSION_NAME")).strip() == "":
+    SESSION_NAME = str(None)
+else:
+    SESSION_NAME = str(getenv("SESSION_NAME"))
+
+if str(getenv("STRING_SESSION2")).strip() == "":
+    SESSION2 = str(None)
+else:
+    SESSION2 = str(getenv("STRING_SESSION2"))
+
+if str(getenv("STRING_SESSION3")).strip() == "":
+    SESSION3 = str(None)
+else:
+    SESSION3 = str(getenv("STRING_SESSION3"))
+
+if str(getenv("STRING_SESSION4")).strip() == "":
+    SESSION4 = str(None)
+else:
+    SESSION4 = str(getenv("STRING_SESSION4"))
+
+if str(getenv("STRING_SESSION5")).strip() == "":
+    SESSION5 = str(None)
+else:
+    SESSION5 = str(getenv("STRING_SESSION5"))
+
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_NAME = getenv("BOT_NAME", "Umk")
+
+API_ID = int(getenv("API_ID", "7311387"))
+API_HASH = getenv("API_HASH", "77449cfdebb052e2f512e69223956e52")
+MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Cloner:Cloner@cluster0.cgc6t.mongodb.net/?retryWrites=true&w=majority")
+OWNER_NAME = getenv("OWNER_NAME", "Meaw The Ronaldo")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "Meawtheronaldo")
+ALIVE_NAME = getenv("ALIVE_NAME", "Fred")
+BOT_USERNAME = getenv("BOT_USERNAME", "Fred_musicbot")
+OWNER_ID = getenv("OWNER_ID", "1978498226")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Fred Music Assistant")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "MEOOWLAND")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "MEOOWLAND")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+HEROKU_API_KEY = getenv("UPDATES_CHANNEL", "HEROKU_API_KEY")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1978498226").split()))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/fc9d87ffd1c6f828eb7fc.png")
+START_PIC = getenv("START_PIC", "https://telegra.ph//file/851335dd9e5d5d15cc90c.jpg")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ITZ-Fred/Fred-Vc-Player")
+PLAY_IMG = getenv("PLAY_IMG", "https://telegra.ph/file/10b1f781170b1e1867f68.png")
+QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/b95c13eef1ebd14dbb458.png")
+CMD_IMG = getenv("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
+VIDEO_IMG = getenv("VIDEO_IMG", "https://telegra.ph/file/6213d2673486beca02967.png")
+SKIP_IMG = getenv("SKIP_IMG", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
+NEXT_IMG = getenv("NEXT_IMG", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
+HEROKU_MODE = getenv("HEROKU_MODE", None)
---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio
+from pytgcalls import idle
+from Fred.Database import db
+
+import os
+import sys
+import random
+import asyncio
+from config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME, SESSION2
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+from Fred.main import *
+
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
